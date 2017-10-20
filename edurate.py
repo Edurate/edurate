a171c6445f9ca31293ba1a0e66737afa3d1248bd
Zachary Andrews <andrewsz@allegheny.edu>
2017-10-20 14:20:13 -0400

    fixed command line issue

--- edurate.py
+++ edurate.py
@@ -6,4 +6,4 @@ from parse_arguments import parse_arguments
 if __name__ == "__main__":
     print("Welcome to Edurate")
     print("https://github.com/Edurate/edurate")
-    edu_args = parse_arguments(sys.argv[0:])
+    edu_args = parse_arguments(sys.argv[1:])
