2de6450f54ded13dcf4335b6bba016b47f0b26fa
burrowss <burrowss@allegheny.edu>
2017-10-23 13:55:10 -0400

    linting.py goes through and prints out linting errors that do not adhere to pep8, will be moved into the test suite like last time later

---
+++ linting.py
@@ -0,0 +1,20 @@
+
+import glob
+import os
+from flake8.api import legacy as flake8
+
+
+# Linting Tests
+def test_flake8():
+
+    # list of all file names to be checked for PEP8
+    filenames = list()
+
+    # fill list with all python files found in all subdirectories
+    for root, dirs, files in os.walk("edurate", topdown=False):
+        pyFiles = glob.glob(root + "/*.py")
+        filenames.extend(pyFiles)
+
+    style_guide = flake8.get_style_guide(ignore=["E265", "E501"])
+    report = style_guide.check_files(filenames)
+    assert report.get_statistics('E') == [], 'Flake8 found violations'
