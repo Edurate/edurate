91dda67c31ad4312200d76780b0b285ee81df753
Zachary Andrews <andrewsz@allegheny.edu>
2017-10-24 14:55:22 -0400

    added debug, spam, and file commands

--- parse_arguments.py
+++ parse_arguments.py
@@ -13,9 +13,29 @@ def parse_arguments(args):
     )
     
     edu_parser.add_argument(
+        "-d", "--debug",
+        help="Display diagnostic information",
+        action="store_const", dest="logging_level",
+        const=logging.DEBUG, default=logging.ERROR
+    )
+    
+    edu_parser.add_argument(
         "-v", "--verbose",
         help="Display confirmation information",
         action="store_const", dest="logging_level", const=logging.INFO
+    )
+    
+    edu_parser.add_argument(
+        "-s", "--spam",
+        help="Removes spam and other unneccessary information",
+        action="store_true", required=False
+    )
+    
+    edu_parser.add_argument(
+        "-f", "--file",
+        help="File location with the responses",
+        type=str,
+        required=False
     )
     
     arguments = edu_parser.parse_args(args)
