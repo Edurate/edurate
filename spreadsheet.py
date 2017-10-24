1525b17203fc11d99c400c00c06dcd806ae0d2ac
Michael Dilla <TheTrainstation@Michaels-MacBook-Pro.local>
2017-10-24 16:46:37 -0400

    creates csv

--- spreadsheet.py
+++ spreadsheet.py
@@ -19,3 +19,35 @@ def read_from_spreadsheet():
     # Extract and return all of the values
     list_of_hashes = sheet.get_all_records()
     return list_of_hashes
+
+def create_csv(spreadsheet_list):
+    # returns True when funciton is completed
+    logging.info("Creating a list of lists of students")
+    formated_list = list()
+    for entry in spreadsheet_list:
+        formated_entry = list()
+        for question, response in entry.items():
+            if question == 'Email Address':
+                username = entry[question].partition('@')[0]
+                formated_entry.append(username)
+            elif question == 'Timestamp':
+                time = entry[question].partition(' ')[0]
+                formated_entry.append(time)
+            else:
+                formated_entry.append(response)
+
+        formated_entry.insert(
+            0, formated_entry.pop(
+                formated_entry.index(time)))
+        formated_entry.insert(
+            1, formated_entry.pop(
+                formated_entry.index(username)))
+        formated_list.append(formated_entry)
+
+    logging.info("Writing formatted data to CSV file")
+    logging.debug("CSV file name: " + "data.csv")
+    with open("./data.csv", 'w') as myfile:
+        writer = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for item in formated_list:
+            writer.writerow(item)
+    return True
