2b19e40eeb23a8268103c778582afffa7a31ce07
Michael Dilla <TheTrainstation@Michaels-MacBook-Pro.local>
2017-10-24 16:45:35 -0400

    added function calls to main

--- edurate.py
+++ edurate.py
@@ -1,6 +1,11 @@
 import sys
 import logging
+from spreadsheet import read_from_spreadsheet
+from spreadsheet import create_csv
 
 if __name__ == "__main__":
     print("Welcome to Edurate")
     print("https://github.com/Edurate/edurate")
+
+    spreadsheet_list = read_from_spreadsheet()
+    create_csv(spreadsheet_list)
