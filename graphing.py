b373e687a1d2db27fb1b6416746ef9b0f0c98d98
[Chris Cook] <[cookc@allegheny.edu]>
2017-10-26 19:00:56 -0400

    pushing graph 2 work

--- graphing.py
+++ graphing.py
@@ -26,12 +26,21 @@ def exampleGraph(data):
 def graph1(data):
     """ Average score as time goes on """
 
-def graph2(data):
+def graph2(data3):
     """ Change over time. """
-    
+
+    # Converting data
+    dat3 = DataFrame(data3, columns = ['A', 'B'])
+
+    # Creating graph then assigning to g3
+    g3 = ggplot(dat3, aes(x='A', y='B')) + geom_line()
+
+    # Return graph
+    return g3
+
 def graph2(data):
     """ Distribution of scores for a given review"""
 
 # Testing out simple graph
-data1 = [[1,1],[2,4],[3,9],[4,16],[5,25],[6,36],[7,49],[8,64],[9,81],[10,100]]
+data3 = [[1,1],[2,4],[3,9],[4,16],[5,25],[6,36],[7,49],[8,64],[9,81],[10,100]]
 print(exampleGraph(data1))
