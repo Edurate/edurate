4d792d0ce04db5b97987393e72b6e4d75ccd512f
toccinAC <toccin@allegheny.edu>
2017-10-30 16:44:17 -0400

    adding the new tokenizer

--- edurate_gensim.py
+++ edurate_gensim.py
@@ -13,13 +13,27 @@ import gensim
 
 def create_tokens(list_responses):
     """Takes in the list of responses and makes each word a token"""
-    texts = [[word for word in list_responses.lower().split()]
-        for list_responses in list_responses]
-    #print(texts)
-    dictionary = corpora.Dictionary(texts)
-    #print(dictionary.token2id)
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    print(corpus)
-    return(corpus)
+    #list_responses.lower()
+    #print(list_responses)
+    stoplist = set('poop high fuck yeah'.split())
+    #print(stoplist)
+    texts = []
+    for i in list_responses:
+        texts.append(i.lower())
+    texts = [[word for word in texts.split()]for texts in texts]
+    tokens = []
+    for i in texts:
+        if len(i)>3:
+            temp = []
+            for i in i:
+                if i not in stoplist:
+                    #if i.isnumeric() == False:
+                        #print(i)
+                    temp.append(i)
+            tokens.append(temp)
+    print(tokens)
+    dictionary = corpora.Dictionary(tokens)
+    corpus = [dictionary.doc2bow(token) for token in tokens]
+    print(dictionary.token2id)
 
 #def corp_eval():
