e1e633938a4d12000631064e4d4cfd5062deed80
JattMones <jonesm2@allegheny.edu>
2017-10-30 17:20:31 -0400

    Update edurate_gensim.py

--- edurate_gensim.py
+++ edurate_gensim.py
@@ -31,9 +31,21 @@ def create_tokens(list_responses):
                         #print(i)
                     temp.append(i)
             tokens.append(temp)
+   # "Removes elements that were not repeated"
+   # frequency = defaultdict(int) <-- Change defaultdict to our dictionary?
+   # for text in texts:
+   #     for token in text:
+   #         frequency[token] += 1
+
+   # texts = [[token for token in text if frequency[token] > 1]
+   #          for text in texts]
     print(tokens)
     dictionary = corpora.Dictionary(tokens)
     corpus = [dictionary.doc2bow(token) for token in tokens]
     print(dictionary.token2id)
+    #User-interaction "Ask user if they'd like to explore the words repeated (Y/N)
+    #If yes, "prompt user to type in the Id number, or press x to exit", pulls that word from the csv file. This 
+    #will work if we change the code so that it assigns all words to a number, and then removes the stop and 
+    #repeated words.
 
 #def corp_eval():
