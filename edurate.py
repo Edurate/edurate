49b05c3e3b626257db7c1532b1f6d03da66542f1
Austin Bristol <bristola@aldenv181.allegheny.edu>
2017-10-31 00:15:11 -0400

    Updated main

--- edurate.py
+++ edurate.py
@@ -8,7 +8,7 @@ from spreadsheet import *
 from parse_arguments import parse_arguments
 from read_responses import read_responses
 from parse_arguments import parse_arguments
-
+from graphing import graph
 
 if __name__ == "__main__":
     print("Welcome to Edurate")
@@ -24,12 +24,10 @@ if __name__ == "__main__":
         print("Graph")
     spreadsheet_list = read_from_spreadsheet()
     data = getGraphData(spreadsheet_list, edu_args.confidential)
-    #print(data)
-    #filterDates(data)
-    create_csv(spreadsheet_list)
-    # sends file name supplied by user and if confidential
-    # and gets the responses from the csv file
-    res = read_responses(edu_args.file, edu_args.confidential)
-    #for response in res:
-        #print(response)
-    print(res)
+    if(edu_args.confidential):
+        print("Confidential")
+    if(edu_args.archive == True):
+        print("Archive")
+    if(edu_args.graph == True):
+        print("Graph")
+        graph(data)
