f86d733ee905109270de098e4f20320c22dae5b3
Jenna Millin <jennamillin@ewire23-128.allegheny.edu>
2017-10-30 22:32:02 -0400

    adding test cases

--- tests/NTTests-millinj2.py
+++ tests/NTTests-millinj2.py
@@ -1,5 +1,20 @@
 import pytest
+
+# Run pytest linting.py to run the program and check code
+# Install autopep8 and pep8
+# pip install autopep8
+# pip install pep8
+# pip install pytest-flake8
+# To fix many issues, type the following into the terminal
+# autopep8 --in-place  --aggressive --aggressive  *.py
+# If it still fails, go to the code that is specified and change it
+import glob
+import os
+from flake8.api import legacy as flake8
+#import parse_arguments
+#import logging
 import graphing
+from ggplot import *
 
 def test_convertToInts():
     """Testing the convertToInts() function"""
@@ -7,4 +22,47 @@ def test_convertToInts():
     desired_output_string = [["Some", "Something", 1, 2, 3]]
     actual_output_string = graphing.convertToInts(list)
     assert len(list) == 5
-    assert(desried_output_string == actual_output_string) is True
+    assert(desired_output_string == actual_output_string) is True
+
+'''
+def test_parse_arguments1():
+    args = []
+    parsed_args = parse_arguments.parse_arguments(args)
+    assert parsed_args.logging_level == logging.ERROR
+
+def test_parse_arguments2():
+    args = ['--file', 'data.csv', '--debug']
+    parsed_args = parse_arguments.parse_arguments(args)
+    assert parsed_args.file == 'data.csv'
+    assert parsed_args.logging_level == logging.DEBUG
+
+def test_parse_arguments3():
+    args = ['--file', 'data.csv', '--confidential']
+    parsed_args = parse_arguments.parse_arguments(args)
+    assert parsed_args.confidential == True
+
+def test_parse_arguments4():
+    args = ['--file', 'data.csv', '--spam']
+    parsed_args = parse_arguments.parse_arguments(args)
+
+    def test_parse_arguments4():
+    args = ['--file', 'data.csv', '--verbose']
+    parsed_args = parse_arguments.parse_arguments(args)
+
+'''
+
+
+# Linting Tests
+def test_flake8():
+
+    # list of all file names to be checked for PEP8
+    filenames = list()
+
+    # fill list with all python files found in all subdirectories
+    for root, dirs, files in os.walk("edurate", topdown=False):
+        pyFiles = glob.glob(root + "/*.py")
+        filenames.extend(pyFiles)
+
+    style_guide = flake8.get_style_guide(ignore=["E265", "E501"])
+    report = style_guide.check_files(filenames)
+    assert report.get_statistics('E') == [], 'Flake8 found violations'
