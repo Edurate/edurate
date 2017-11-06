ff9c5e9e013cdb0635fc1e0b6fc97bc27229fff1
yeeunmariakim <yeeunmariakim@gmail.com>
2017-11-05 20:05:21 -0500

    removing archiving ability

--- edurate.py
+++ edurate.py
@@ -21,17 +21,14 @@ if __name__ == "__main__":
     edu_args = parse_arguments(sys.argv[1:])
     spreadsheet_list = read_from_spreadsheet()
     data = getGraphData(spreadsheet_list, edu_args.confidential)
-    archive_info = data
-    if(edu_args.archive):
-        print("Archiving Information...")
-        archive_information(filterDates(list(archive_info)))
+
     if(edu_args.graph):
         print("Creating Graphs...")
         graph(data)
 
     create_csv(spreadsheet_list)
-    res = read_responses(edu_args.file, edu_args.confidential)
-    res = flip_responses(data)
+    res = filterDates(read_responses(edu_args.file, edu_args.confidential))
+    res = flip_responses(res)
     # for response in res:
     #     print(response)
     q_count = 7
