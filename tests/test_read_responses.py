b01ab6b31ae29f6e0f41c7c0398892406ee3c9f9
lylet-AC <lylet@allegheny.edu>
2017-11-06 11:34:59 -0500

    added testcase for a bad filepath

--- tests/test_read_responses.py
+++ tests/test_read_responses.py
@@ -1,3 +1,10 @@
 import read_responses
+"""tests the functionality of read_responses.py"""
 
-def test_read_responses():
+#def test_read_responses():
+	
+	
+def test_no_filepath():
+	"""tests what happens when no meaningful filepath is specified"""
+	result = read_responses.read_responses("", False)
+	assert result == "File Not Found"
